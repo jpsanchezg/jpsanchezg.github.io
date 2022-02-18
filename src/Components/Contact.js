f694a796723c6dd94f2297f6e411d0929583fb89
juan Pablo Sanchez Gaitan <Juansanchezg@javeriana.edu.co>
2022-02-17 20:39:03 -0500

    update nodemailer

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import Swal from 'sweetalert2';
-import emailjs from 'emailjs-com';
-import{ init } from '@emailjs/browser';
-init("user_8U6FLi1UshWAFJKiQWKsW");
 
-
-const SERVICE_ID = "service_2jmhj79";
-const TEMPLATE_ID = "template_0lio3mq";
-const USER_ID = "user_8U6FLi1UshWAFJKiQWKsW";
 class Contact extends Component {
 
 
@@ -26,25 +19,6 @@ class Contact extends Component {
       }
 
 
-      const handleOnSubmit = (e) => {
-         e.preventDefault();
-         emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-            .then((result) => {
-               console.log(result.text);
-               Swal.fire({
-                  icon: 'success',
-                  title: 'Message Sent Successfully'
-               })
-            }, (error) => {
-               console.log(error.text);
-               Swal.fire({
-                  icon: 'error',
-                  title: 'Ooops, something went wrong',
-                  text: error.text,
-               })
-            });
-         e.target.reset()
-      };
 
       return (
          <section id="contact">
@@ -68,7 +42,7 @@ class Contact extends Component {
             <div className="row">
                <div className="eight columns">
 
-                  <form action="" id="contactForm" name="contactForm" onSubmit={handleOnSubmit}>
+                  <form action="" id="contactForm" name="contactForm" >
                      <fieldset>
 
                         <div>
